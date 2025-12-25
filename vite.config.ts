242bdf1f176f0019935324a6b24087ccbc83a4d4
borick <boris@sitsker.com>
2025-12-25 15:54:17 -0500

    a bunch of shit

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: './',
-      plugins: [react()],
-      build: {
-          outDir: 'dist',
-          rollupOptions: {
-              input: {
-                  main: './index.html',
-              },
-          },
-      },
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
+      plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
