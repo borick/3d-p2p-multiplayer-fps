826bf9799f785f722ea89ef4a072db367969c481
borick <boris@sitsker.com>
2025-12-25 15:57:11 -0500

    Yes

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: './',
+      build: {
+          outDir: 'dist',
+          rollupOptions: {
+              input: {
+                  main: './index.html',
+              },
+          },
+      },
       server: {
         port: 3000,
         host: '0.0.0.0',
